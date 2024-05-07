e5232488d44de90b1429d452a50e3afe095cafde
mahamad008 <mahamdabdihassan008@gmail.com>
2024-05-07 14:40:00 +0300

    first commit

--- Data/ApplicationDbcontext.cs
+++ Data/ApplicationDbcontext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace traingbackendc_.Data
 {
-    public class ApplicationDbcontext:Dbcontext
+    // public class ApplicationDbcontext:Dbcontext
     {
         public ApplicationDbcontext(DbConnection<ApplicationDbcontext> options):base(options)
         {
