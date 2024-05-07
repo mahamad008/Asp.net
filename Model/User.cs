192a47a96143d1c6c2a4ebd341178f397a97f71b
mahamad008 <mahamdabdihassan008@gmail.com>
2024-05-07 14:33:02 +0300

    first commit

---
+++ Model/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace traingbackendc_.Model
+{
+    public class User
+    {
+    
+    }
+}
--- obj/Debug/net8.0/traingbackendc#.AssemblyInfo.cs
+++ obj/Debug/net8.0/traingbackendc#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("traingbackendc#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07a343d1c4c9736e83f29eb88292aca51d6007e")]
 [assembly: System.Reflection.AssemblyProductAttribute("traingbackendc#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("traingbackendc#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
