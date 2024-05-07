2a177939c48590c467332d1ab35f833817380f09
mahamad008 <mahamdabdihassan008@gmail.com>
2024-05-07 14:36:01 +0300

    first commit

--- Model/User.cs
+++ Model/User.cs
@@ -7,6 +7,8 @@ namespace traingbackendc_.Model
 {
     public class User
     {
-    
+     public int id {get;set;}
+     public string Name {get;set;}
+     public string email {get;set;}
     }
 }
