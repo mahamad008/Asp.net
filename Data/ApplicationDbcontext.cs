3108de0823fccb7919b3bbb6aace03af0e956114
mahamad008 <mahamdabdihassan008@gmail.com>
2024-05-07 14:47:24 +0300

    seocnd commit

--- Data/ApplicationDbcontext.cs
+++ Data/ApplicationDbcontext.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace traingbackendc_.Data
 {
     public class ApplicationDbcontext:Dbcontext
-    
+    {
         public ApplicationDbcontext(DbConnection<ApplicationDbcontext> options):base(options)
         {
 
