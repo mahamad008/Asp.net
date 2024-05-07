fa0b12830e047e8fc2961506c8175b0a4abc7c1b
mahamad008 <mahamdabdihassan008@gmail.com>
2024-05-07 14:42:42 +0300

    first commit

--- Data/ApplicationDbcontext.cs
+++ Data/ApplicationDbcontext.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 
-namespace traingbackendc_.Data
-{
-    public class ApplicationDbcontext:Dbcontext
-    {
-        public ApplicationDbcontext(DbConnection<ApplicationDbcontext> options):base(options)
-        {
+// namespace traingbackendc_.Data
+// {
+//     public class ApplicationDbcontext:Dbcontext
+//     {
+//         public ApplicationDbcontext(DbConnection<ApplicationDbcontext> options):base(options)
+//         {
 
-        }
-        public Dbset<User> Users { get; set; }
-    }
-}
+//         }
+//         public Dbset<User> Users { get; set; }
+//     }
+// }
